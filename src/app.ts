bf41a7b937f161a67d0ccb6fa26c56407ee33767
whazys <santos_rafa1973@hotmail.com>
2022-01-22 11:57:21 -0300

    DoWhile - Node

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,41 @@
-import "dotenv/config"
-import express from "express"
-import { router } from "./routes"
+import "dotenv/config";
+import express from "express";
+import http from "http";
+import cors from "cors";
 
+import { Server } from "socket.io";
 
-const app = express()
-app.use(express.json())
+import { router } from "./routes";
 
-app.use(router)
+const app = express();
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+    cors: {
+        origin: "*",
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log(`Usuário conectado no socket ${socket.id}`);
+});
+
+app.use(express.json());
+
+app.use(router);
 
 app.get("/github", (request, response) => {
-    response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
-})
+    response.redirect(
+        `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+    );
+});
 
 app.get("/signin/callback", (request, response) => {
     const { code } = request.query;
-    return response.json(code)
-})
 
-app.listen(4000, () => console.log(`Server is running on PORT 4000` ))
+    return response.json(code);
+});
+
+export { serverHttp, io };
